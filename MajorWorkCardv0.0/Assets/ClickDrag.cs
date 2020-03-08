c34b2f4aef9c2ea7338c0bd15bb9afb18214a70d
Shlapsham <shlapsham@gmail.com>
2020-03-08 16:33:20 +1100

    Start

---
+++ MajorWorkCardv0.0/Assets/ClickDrag.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickDrag : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+    private void OnMouseDown()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+
+        }
+    }
+}

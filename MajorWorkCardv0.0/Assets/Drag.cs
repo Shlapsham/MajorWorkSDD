2d237f1129b85dddd2a00f51ed2a8aab75816ee5
Shlapsham <shlapsham@gmail.com>
2020-03-10 14:24:20 +1100

    v0.01
    Doing card position in hand

---
+++ MajorWorkCardv0.0/Assets/Drag.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drag : MonoBehaviour
+{
+    [SerializeField]
+    bool holding = false;
+    float positionOf1in10 = -250; //x pos of the first card in a hand of 10
+    float spaceBetween = 50; //space inbetweeen cards
+    [SerializeField]
+    float rowPosition;
+    [SerializeField]
+    float cardsInHand;
+    Vector2 currentPos;
+    Vector2 basePos;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        currentPos = (Vector2)transform.position;
+        mouseDrag();
+        returnToBase();
+    }
+    private void OnMouseDown()
+    {
+        holding = !holding;
+    }
+    void mouseDrag()
+    {
+        if (holding)
+        {
+            transform.position = Vector2.Lerp(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), 0.8f);
+        }
+    }
+    void returnToBase()
+    {
+        basePos = new Vector2((spaceBetween * (10f - cardsInHand) * rowPosition) / 2 + positionOf1in10, -100);
+        if (!holding && currentPos != basePos)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, basePos, 25);
+        }
+    }
+}
